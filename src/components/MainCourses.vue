0227e913ff1fa5ea92ef018b946aeae2afe89079
Nicowski25 <nicola.faedo0610@gmail.com>
2023-04-27 12:06:27 +0200

    main courses and onlinelearning section

---
+++ src/components/MainCourses.vue
@@ -0,0 +1,98 @@
+<script>
+export default {
+    name: 'MainCourses'
+}
+</script>
+<template>
+    <div class="container w-75 p-0 py-5">
+        <div class="containergrid">
+            <div class="main">
+                <img src="../assets/img/cat_1-1540x750.jpg">
+                <div class="info">
+                    <h5>Software Developement</h5>
+                    <p>5 courses</p>
+                </div>
+            </div>
+            <div class="side1">
+                <img src="../assets/img/cat_2-544x322.jpg" height="138px" width="330px">
+                <div class="info">
+                    <h5>Software Developement</h5>
+                    <p>5 courses</p>
+                </div>
+            </div>
+            <div class="side2">
+                <img src="../assets/img/cat_3-740x310.jpg">
+                <div class="info">
+                    <h5>Software Developement</h5>
+                    <p>5 courses</p>
+                </div>
+            </div>
+            <div class="side3">
+                <img src="../assets/img/cat_4-740x310.jpg">
+                <div class="info">
+                    <h5>Software Developement</h5>
+                    <p>5 courses</p>
+                </div>
+            </div>
+            <div class="bot1">
+                <img src="../assets/img/cat_5-740x310.jpg">
+                <div class="info">
+                    <h5>Software Developement</h5>
+                    <p>5 courses</p>
+                </div>
+            </div>
+            <div class="bot2">
+                <img src="../assets/img/cat_6-740x310.jpg">
+                <div class="info">
+                    <h5>Software Developement</h5>
+                    <p>5 courses</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.info {
+    display: flex;
+    justify-content: space-between;
+}
+
+img {
+    max-width: 100%;
+}
+
+.containergrid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 10px 15px;
+    grid-auto-flow: row;
+}
+
+.main {
+    grid-area: 1 / 1 / 3 / 3;
+}
+
+.side1 {
+    grid-area: 1 / 3 / 2 / 4;
+}
+
+.side2 {
+    grid-area: 2 / 3 / 3 / 4;
+}
+
+.side3 {
+    grid-area: 3 / 3 / 4 / 4;
+}
+
+.bot1 {
+    grid-area: 3 / 1 / 4 / 2;
+}
+
+.bot2 {
+    grid-area: 3 / 2 / 4 / 3;
+}
+
+</style>
---
+++ src/components/MainOnlineLearning.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'MainOnlineLearning'
+}
+</script>
+<template>
+    <div class="container-fluid d-flex py-5">
+        <div class="container w-75 p-0">
+            <h2>Limitless learning, more possibilities</h2>
+            <p class="py-3">Online courses open the opportunity for learning to almost anyone, <br> regardless of their scheduling commitments</p>
+            <button class="main-btn">READ MORE</button>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables.scss' as *;
+.container-fluid {
+    background-color: $light-grey;
+    background-image: url('../assets/img/Untitled-1-1-1-1-1.png');
+    background-position: center;
+    background-size: cover;
+}
+
+</style>
